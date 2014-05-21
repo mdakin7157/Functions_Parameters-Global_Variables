ebe66b82df21dd78dc2da92063cc190421dfa0d3
mdakin7157 <mdakin7157@users.noreply.github.com>
2014-05-21 23:07:50 +0100

    Update diceRoller.py
    this is all i could do. i attempted the roll until a 6 thing but couldnt get it to work.

--- diceRoller.py
+++ diceRoller.py
@@ -1,22 +1,25 @@
-#Coin flip program
-#Describe the purpose of this program here.
+#dice roll program
+#the purpose of this program is to roll a dice and present it to the user as if it was an actual dice using ASCII art.
 
 import random
 import time
+
 s1 = "- - - - -\n|       |\n|   O   |\n|       |\n- - - - -\n"
 s2 = "- - - - -\n| O     |\n|       |\n|     O |\n- - - - -\n"
 s3 = "- - - - -\n| O     |\n|   O   |\n|     O |\n- - - - -\n"
 s4 = "- - - - -\n| O   O |\n|       |\n| O   O |\n- - - - -\n"
 s5 = "- - - - -\n| O   O |\n|   O   |\n| O   O |\n- - - - -\n"
 s6 = "- - - - -\n| O   O |\n| O   O |\n| O   O |\n- - - - -\n"
-
+x = 0
 def roll():
     print("rolling...")
     roll = random.randint(1,6)
     return roll
 
+    
 
 def show_dice(roll):
+    global x
     if roll == 1:
         print(s1)
     elif roll == 2:
@@ -27,9 +30,20 @@ def show_dice(roll):
         print(s4)
     elif roll == 5:
         print(s5)
-    elif roll == 6:
+    else:
         print(s6)
+        x = x + 10
+    return myroll
+    return x
 
-roll
+User_Input = input("type 'roll' to roll the dice. ")
+if User_Input == "roll":
+    myroll = roll()
+else:
+    quit
+
+    
+
 time.sleep(1)
-show_dice(roll)
+
+show_dice(myroll)
